82a6827bfd2191ac99ef510e22ac6539560d5722
kim-jane <70866712+kim-jane@users.noreply.github.com>
2020-12-08 18:54:18 -0500

    sc

--- src/main.cpp
+++ src/main.cpp
@@ -7,7 +7,9 @@ int main(){
     MPI_Init(NULL, NULL);
 
     TestIsing2D test;
-    test.equilibrate_sweepK1(1E6, 64, 0.4, 0.5, 0.01);
+    test.equilibrate_sweepK1(1E5, 32, -0.50, -0.40, 0.01);
+    test.equilibrate_sweepK1(1E5, 64, -0.50, -0.40, 0.01);
+    test.equilibrate_sweepK1(1E5, 128, -0.50, -0.40, 0.01);
 
     MPI_Finalize();
     return 0;
