2638f3fd4bab0be1d9d0fd45c84e167f0bcb51e8
kim-jane <70866712+kim-jane@users.noreply.github.com>
2020-12-10 00:07:18 -0500

    s

--- src/main.cpp
+++ src/main.cpp
@@ -14,7 +14,7 @@ int main(){
     MonteCarloRenormalizationGroup MCRG(b);
     //double nu = MCRG.calc_critical_exponent(1E6, 32, Kc);
     
-    vec2D Kc_approx = MCRG.locate_critical_point(1E6, 64, Kc);
+    vec2D Kc_approx = MCRG.locate_critical_point(10, 1E6, 64, Kc);
     
     MPI_Finalize();
     return 0;
