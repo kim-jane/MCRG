fac2abc1e42040f6e0ef48aafab60e856a368b8a
kim-jane <70866712+kim-jane@users.noreply.github.com>
2020-12-13 20:01:14 -0500

    sc

--- src/main.cpp
+++ src/main.cpp
@@ -20,8 +20,10 @@ int main(){
     
     MonteCarloRenormalizationGroup MCRG(b);
     
-    Kc = MCRG.locate_critical_point(100, 1E4, 1E6, 64, Kc);
-    MCRG.calc_critical_exponent(10, 1E5, 1E6, 64, Kc);
+    Kc = MCRG.locate_critical_point(5, 1E4, 1E6, 32, Kc);
+    Kc = MCRG.locate_critical_point(5, 5E4, 1E6, 32, Kc);
+    Kc = MCRG.locate_critical_point(5, 1E5, 1E6, 32, Kc);
+    //MCRG.calc_critical_exponent(10, 1E5, 1E6, 64, Kc);
     
     MPI_Finalize();
     return 0;
