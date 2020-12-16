37b19b52c0e9517b6b62d6bee11e058a8910cdc3
kim-jane <70866712+kim-jane@users.noreply.github.com>
2020-12-16 01:11:52 -0500

    more samples

--- src/main.cpp
+++ src/main.cpp
@@ -10,13 +10,12 @@ int main(){
     int n_samples_eq = 1E4;
     int n_samples = 1E7;
     int n_iterations = 10;
-    double K0 = -0.40;
     double Kc = -log(1+sqrt(2))/2;
 
     MonteCarloRenormalizationGroup MCRG(b);
     MCRG.calc_critical_exponent(n_samples_eq, n_samples, N, Kc);
     
-    double Kc_N = MCRG.locate_critical_point(n_iterations, n_samples_eq, n_samples, N, K0);
+    double Kc_N = MCRG.locate_critical_point(n_iterations, n_samples_eq, n_samples, N, Kc);
     MCRG.calc_critical_exponent(n_samples_eq, n_samples, N, Kc_N);
     
     
