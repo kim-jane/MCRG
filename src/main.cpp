4956ab6cb91c17281c2b08aeb3de40e3de54368d
kim-jane <70866712+kim-jane@users.noreply.github.com>
2020-12-16 03:10:28 -0500

    everything works perfectly

--- src/main.cpp
+++ src/main.cpp
@@ -9,17 +9,15 @@ int main(){
     int N = 64;
     int n_samples_eq = 1E4;
     int n_samples = 1E6;
-    int n_iterations = 10;
-    double Kc = -0.44;
-    //double Kc = -log(1+sqrt(2))/2;
+    int n_iterations = 20;
+    double K0 = -0.44;
+    double Kc = -log(1+sqrt(2))/2;
 
     MonteCarloRenormalizationGroup MCRG(b);
     
-    Kc = MCRG.locate_critical_point(n_iterations, n_samples_eq, n_samples, N, Kc);
-
-    //double Kc_N = MCRG.locate_critical_point(n_iterations, n_samples_eq, n_samples, N, Kc);
-    //MCRG.calc_critical_exponent(n_samples_eq, n_samples, N, Kc_N);
-    
+    Kc_N = MCRG.locate_critical_point(n_iterations, n_samples_eq, n_samples, N, K0);
+    MCRG.calc_critical_exponent(n_samples_eq, n_samples, N, Kc_N);
+    MCRG.calc_critical_exponent(n_samples_eq, n_samples, N, Kc);
     
     MPI_Finalize();
     return 0;
