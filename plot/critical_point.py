1374a201463cca30c141d060d39cc77a58367e96
kim-jane <70866712+kim-jane@users.noreply.github.com>
2020-12-17 12:49:20 -0500

    plot critical coupling done

--- plot/critical_point.py
+++ plot/critical_point.py
@@ -2,9 +2,14 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 b = 2
-L = [32, 64, 128]
-colors = ["b", "g", ""]
+L = [16, 32, 64, 128]
+colors = ["indianred", "orange", "forestgreen", "royalblue"]
 data = []
+
+plt.figure(figsize=(8,6))
+
+Kc = -np.log(1+np.sqrt(2))/2
+plt.hlines(Kc, xmin=-10, xmax=100, colors='k', label=r"True $K_1^c$ = "+str(round(Kc,6)))
 
 for l in L:
 
@@ -13,7 +18,17 @@ for l in L:
     
 for i in range(len(L)):
     
-    plt.plot(data[i][0], data[i][2], label="L = "+str(L[i])+", S = "+str(round(L[i]/b)))
+    plt.plot(data[i][0], data[i][2], label="L = "+str(L[i])+", S = "+str(round(L[i]/b)), c=colors[i], linewidth=2)
+    
+    n = int(50*(np.log2(L[i])-1))
+    avg = np.average(data[i][2][-n:])
+    plt.hlines(avg, xmin=-10, xmax=100, linestyle="--", colors=colors[i], label=r"$K_1^c$ = "+str(round(avg,6)))
+    
+plt.hlines(Kc, xmin=-10, xmax=100, colors='k', linestyle="--", label=r"Avg $K_1^c$ over last 50 iterations")
 
-plt.legend()
-plt.show()
+plt.title(r"Locating critical nearest neighbor coupling $K_1^c$")
+plt.ylabel(r"$K_1^c$")
+plt.xlabel(r"Iteration")
+plt.xlim(0, 85)
+plt.legend(ncol=2)
+plt.savefig("critical_point.pdf", format="pdf")
--- src/main.cpp
+++ src/main.cpp
@@ -9,19 +9,12 @@ int main(){
     int N = 16;
     int n_samples_eq = 1E4;
     int n_samples = 1E6;
-    int n_iterations = 100;
-    double K0 = -0.44;
-    double Kc = -log(1+sqrt(2))/2;
-    double Kc_N;
+    int n_iterations = 80;
+    double K = -0.440415;
 
     MonteCarloRenormalizationGroup MCRG(b);
 
-    // calc thermal exponent at critical coupling for N->inf
-    MCRG.calc_critical_exponent(n_samples_eq, n_samples, N, Kc);
-    
-    // locate critical point for finite N
-    Kc_N = MCRG.locate_critical_point(n_iterations, n_samples_eq, n_samples, N, K0);
-
+    MCRG.calc_critical_exponent(n_samples_eq, n_samples, N, K);
     
     MPI_Finalize();
     return 0;
