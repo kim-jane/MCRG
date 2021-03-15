1cb6eccdc9395b3932f1a9590dad58ce7e50c290
kim-jane <70866712+kim-jane@users.noreply.github.com>
2021-03-15 14:39:03 -0400

    more samples

--- src/train.cpp
+++ src/train.cpp
@@ -5,9 +5,9 @@ int main(){
     MPI_Init(NULL, NULL);
     
     int b = 2;
-    int N = 32;
+    int N = 64;
     int n_cycles = 1E4;
-    int n_samples = 1E5;
+    int n_samples = 1E6;
     int n_samples_eq = 1E3;
     double Tc = 2/log(1+sqrt(2));
     double h = 0.01;
