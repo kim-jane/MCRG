49eac94a4d650447da258ef5ef6a79b4ac2f157c
kim-jane <70866712+kim-jane@users.noreply.github.com>
2021-03-15 16:41:31 -0400

    relu works best

--- src/train.cpp
+++ src/train.cpp
@@ -6,16 +6,18 @@ int main(){
     
     int b = 2;
     int N = 8;
-    int n_cycles = 1E3;
+    int n_cycles = 5000;
     int n_samples = 1E5;
     int n_samples_eq = 1E4;
-    double Tc = 2.2;
+    double Tc = 2/log(1+sqrt(2));
     double h = 0.0001;
     double eta = 0.001;
     
     RenormalizationGroupNeuralNetwork RGNN(b);
     
     RGNN.train(N, n_cycles, n_samples, n_samples_eq, Tc, h, eta);
+    
+    std::cout << RGNN.W_ << std::endl;
     
     MPI_Finalize();
     return 0;
